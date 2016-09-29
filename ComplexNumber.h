7db3317cc66d690f1c74b5231adc2706008ba48c
unbeman <unbeman@gmail.com>
2016-09-30 02:00:11 +0300

    Update ComplexNumber.h

--- ComplexNumber.h
+++ ComplexNumber.h
@@ -12,19 +12,26 @@ struct ComplexNumber {
     ComplexNumber(double Re = 0, double Im = 0);
     ComplexNumber(const ComplexNumber & number);
 
-    ComplexNumber &operator+(const ComplexNumber &number);
-    friend ComplexNumber operator+(ComplexNumber number, double n);
+    double getRe(const ComplexNumber number);
+    double getIm(const ComplexNumber number);
 
-    friend ComplexNumber operator-(const ComplexNumber n1, const ComplexNumber n2);
+
+    ComplexNumber &operator+(const ComplexNumber &number);
+    ComplexNumber &operator-(const ComplexNumber &number);
+    ComplexNumber &operator*(const ComplexNumber &number);
+
 
     void print(ComplexNumber & number);
     friend ostream &operator<<(ostream & stream, ComplexNumber & number);
     friend istream &operator>>(istream & stream, ComplexNumber & number);
 
     friend bool operator==(ComplexNumber n1, ComplexNumber  n2);
+    friend bool operator!=(ComplexNumber n1, ComplexNumber  n2);
 
     void swap(ComplexNumber & number);
 
+
+private:
     double Re;
     double Im;
 };
