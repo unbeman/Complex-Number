5ff28715a35b30d691d3f34176e6289ffd1e9d6c
Ann <unbeman@gmail.com>
2016-10-04 14:04:25 +0300

    add new methods abs and phase

--- ComplexNumber.cpp
+++ ComplexNumber.cpp
@@ -1,21 +1,35 @@
 //
 // Created by devernua on 28.09.16.
 //
+#include <iostream>
 
+using namespace std;
+
+#include <cmath>
 #include "ComplexNumber.h"
 
-ComplexNumber::ComplexNumber(double Re, double Im){
+ComplexNumber::ComplexNumber(const double Re, const double Im, const double eps){
     this->Re = Re;
     this->Im = Im;
+    this->eps = eps;
 }
 
 ComplexNumber::ComplexNumber(const ComplexNumber & number){
     Re = number.Re;
     Im = number.Im;
+    eps = number.eps;
 }
 
-double ComplexNumber::getRe(ComplexNumber number){ return number.Re;}
-double ComplexNumber::getIm(ComplexNumber number){ return number.Im;}
+double ComplexNumber::getRe() const { return Re;}
+double ComplexNumber::getIm() const { return Im;}
+
+double ComplexNumber::abs() const { return sqrt(Re * Re + Im * Im);}
+double ComplexNumber::phase() const {
+    if (std::abs(Re) < eps){
+        return 0;
+    }
+    return atan(Im / Re); //только для 1 и 4 четвертей
+}
 
 
 ComplexNumber & ComplexNumber::operator+(const ComplexNumber & number){
@@ -31,28 +45,21 @@ ComplexNumber & ComplexNumber::operator-(const ComplexNumber & number){
 }
 
 ComplexNumber & ComplexNumber::operator*(const ComplexNumber & number){
-    Re = Re*number.Re - Im*number.Im;
-    Im = Re*number.Im + number.Re*Im;
+    Re = Re * number.Re - Im * number.Im;
+    Im = Re * number.Im + number.Re * Im;
     return  *this;
 }
 
-
-void ComplexNumber::print(ComplexNumber & number){
-    if (number.Im == 0){
-        cout << number.Re << endl;
-    }
-    else{
-        cout << number.Re << ((number.Im)? '+' : '-') << number.Im << 'i' << endl;
-    }
-}
-
-ostream &operator<<(ostream & stream, ComplexNumber & number){
+ostream &operator<<(ostream & stream, const ComplexNumber & number){
 
     if (number.Im == 0){
-        stream << number.Re << endl;
+        stream << number.Re;
+    }
+    else if( number.Im == 1 || number.Im == -1){
+        stream << number.Re << ((number.Im >= 0)? "+" : "-")  << "i";
     }
     else{
-        stream << number.Re << ((number.Im)? '+' : '-') << number.Im << 'i' << endl;
+        stream << number.Re << ((number.Im >= 0)? "+" : "") << number.Im << "i";
     }
 
     return stream;
@@ -68,18 +75,13 @@ void  ComplexNumber::swap(ComplexNumber & number){
     std::swap(Re, number.Re);
     std::swap(Im, number.Im);
 }
-/* )   ComplexNumber &operator=(ComplexNumber & number){
-    if (this != &number){
-        ComplexNumber(number).swap(*this);
-    }
-    return *this;
-}
-*/
-bool operator==(ComplexNumber n1, ComplexNumber  n2){
-    return (n1.Re == n2.Re) && (n1.Im == n2.Im);
+
+bool operator==(const ComplexNumber n1, const ComplexNumber  n2){
+    double eps = min(n1.eps, n2.eps);
+    return (std::abs(n1.Re - n2.Re) < eps) && ( std::abs(n1.Im - n2.Im) < eps);
 }
 
-bool operator!=(ComplexNumber n1, ComplexNumber  n2){
+bool operator!=(const ComplexNumber n1, const ComplexNumber  n2){
     return !(n1 == n2);
 }
 
--- ComplexNumber.h
+++ ComplexNumber.h
@@ -5,32 +5,32 @@
 #ifndef HOMEWORK_COMPLEXNUMBER_H
 #define HOMEWORK_COMPLEXNUMBER_H
 
-#include <iostream>
-using namespace std;
-
 struct ComplexNumber {
-    ComplexNumber(double Re = 0, double Im = 0);
+    ComplexNumber(const double Re = 0, const double Im = 0, const double eps = 0.0000005);
     ComplexNumber(const ComplexNumber & number);
 
-    double getRe(const ComplexNumber number);
-    double getIm(const ComplexNumber number);
+    double getRe() const;
+    double getIm() const;
+
+    double abs() const;
+    double phase() const;
 
     ComplexNumber &operator+(const ComplexNumber &number);
     ComplexNumber &operator-(const ComplexNumber &number);
     ComplexNumber &operator*(const ComplexNumber &number);
 
-    void print(ComplexNumber & number);
-    friend ostream &operator<<(ostream & stream, ComplexNumber & number);
+    friend ostream &operator<<(ostream & stream, const ComplexNumber & number);
     friend istream &operator>>(istream & stream, ComplexNumber & number);
 
-    friend bool operator==(ComplexNumber n1, ComplexNumber  n2);
-    friend bool operator!=(ComplexNumber n1, ComplexNumber  n2);
+    friend bool operator==(const ComplexNumber n1, const ComplexNumber  n2);
+    friend bool operator!=(const ComplexNumber n1, const ComplexNumber  n2);
 
     void swap(ComplexNumber & number);
 
 private:
     double Re;
     double Im;
+    double eps;
 };
 
 #endif //HOMEWORK_COMPLEXNUMBER_H
